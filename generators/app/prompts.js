76ac844614679198a85d8c6f2175756bf4be5a73
sendilkumarn <sendilkumarn@live.com>
2020-06-19 23:39:58 +0200

    feat: add cypress initial commit

--- generators/app/prompts.js
+++ generators/app/prompts.js
@@ -134,6 +134,7 @@ function askForTestOpts(meta) {
     if (meta || !this.skipClient) {
         // all client side test frameworks should be added here
         choices.push({ name: 'Protractor', value: 'protractor' });
+        choices.push({ name: 'Cypress **In Developement**', value: 'cypress' });
     }
     const PROMPT = {
         type: 'checkbox',
