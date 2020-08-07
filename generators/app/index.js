c5ed03f860e8abf2b3002fbb961fc4fab1b2555f
Anthony Viard <anth.viard@gmail.com>
2020-08-07 17:33:14 +0200

    Rename e2e folder and compose from client subGen insteadOf app

--- generators/app/index.js
+++ generators/app/index.js
@@ -367,14 +367,6 @@ module.exports = class extends BaseBlueprintGenerator {
                         debug: this.isDebugEnabled,
                     });
                 }
-                if (!this.configOptions.skipComposeE2E) {
-                    this.configOptions.skipComposeE2E = true;
-                    this.composeWith(require.resolve('../e2e'), {
-                        ...options,
-                        configOptions,
-                        debug: this.isDebugEnabled,
-                    });
-                }
             },
 
             askForTestOpts: prompts.askForTestOpts,
